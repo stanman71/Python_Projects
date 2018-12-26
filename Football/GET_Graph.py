9734bba656c91aa8d27b3562c84842a6a6d1d7ef
stanman71 <stanlay@gmx.net>
2018-12-26 20:29:16 +0100

    add bootstrap

--- Football/GET_Graph.py
+++ Football/GET_Graph.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap 
+
 from GET_Calc import GET_POINTS
 import matplotlib.pyplot as plt
 import io
 import base64
 
 
+
 app = Flask(__name__)
+Bootstrap(app)
 
 
 import pandas as pd
@@ -29,7 +33,6 @@ def build_graph(x_coordinates, y_coordinates):
 
 @app.route('/') # Change URL
 def graphs():
-    #These coordinates could be stored in DB
 
     x1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17] 
     y1 = GET_POINTS("Borussia Dortmund", df)[3]
@@ -44,9 +47,11 @@ def graphs():
     graph3_url = build_graph(x3,y3);
  
     return render_template('index.html',
-    graph1=graph1_url,
-    graph2=graph2_url,
-    graph3=graph3_url)
+                            graph1=graph1_url,
+                            graph2=graph2_url,
+                            graph3=graph3_url,
+                            site1="sitegfgf1", 
+                            site2="sitegfgf2")
 
 
 if __name__ == '__main__':
