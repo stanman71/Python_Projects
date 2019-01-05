85727f718053cd88f67d7b81243104fbf282cce0
stanman71 <stanlay@gmx.net>
2019-01-05 15:01:07 +0100

    add new poisson

---
+++ Football/NEW/poisson.py
@@ -0,0 +1,177 @@
+# https://dashee87.github.io/football/python/predicting-football-results-with-statistical-modelling/
+# https://github.com/dashee87/blogScripts/blob/master/Jupyter/2017-06-04-predicting-football-results-with-statistical-modelling.ipynb
+# http://www.football-data.co.uk/germanym.php
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn
+from scipy.stats import poisson,skellam
+
+
+""" ########### """
+""" Import data """
+""" ########### """
+
+epl_1617 = pd.read_csv("http://www.football-data.co.uk/mmz4281/1617/E0.csv")
+epl_1617 = epl_1617[['HomeTeam','AwayTeam','FTHG','FTAG']]
+epl_1617 = epl_1617.rename(columns={'FTHG': 'HomeGoals', 'FTAG': 'AwayGoals'})
+
+print(epl_1617.head())
+
+""" Average goals """
+
+epl_1617 = epl_1617[:-10]
+
+print(epl_1617.mean())
+
+
+
+""" ##################### """
+""" Poisson for all goals """
+""" ##################### """
+
+# construct Poisson  for each mean goals value
+poisson_pred = np.column_stack([[poisson.pmf(i, epl_1617.mean()[j]) for i in range(8)] for j in range(2)])
+
+# plot histogram of actual goals
+plt.hist(epl_1617[['HomeGoals', 'AwayGoals']].values, range(9), 
+         alpha=0.7, label=['Home', 'Away'],normed=True, color=["#FFA07A", "#20B2AA"])
+
+# add lines for the Poisson distributions
+pois1, = plt.plot([i-0.5 for i in range(1,9)], poisson_pred[:,0],
+                  linestyle='-', marker='o',label="Home", color = '#CD5C5C')
+pois2, = plt.plot([i-0.5 for i in range(1,9)], poisson_pred[:,1],
+                  linestyle='-', marker='o',label="Away", color = '#006400')
+
+leg=plt.legend(loc='upper right', fontsize=13, ncol=2)
+leg.set_title("Poisson           Actual        ", prop = {'size':'14', 'weight':'bold'})
+
+plt.xticks([i-0.5 for i in range(1,9)],[i for i in range(9)])
+plt.xlabel("Goals per Match",size=13)
+plt.ylabel("Proportion of Matches",size=13)
+plt.title("Number of Goals per Match (EPL 2016/17 Season)",size=14,fontweight='bold')
+plt.ylim([-0.004, 0.4])
+plt.tight_layout()
+plt.show()
+
+
+""" ####################### """
+""" Poisson for home / away """
+""" ####################### """
+
+# probability of draw between home and away team
+skellam.pmf(0.0,  epl_1617.mean()[0],  epl_1617.mean()[1])
+
+# probability of home team winning by one goal
+skellam.pmf(1,  epl_1617.mean()[0],  epl_1617.mean()[1])
+
+skellam_pred = [skellam.pmf(i,  epl_1617.mean()[0],  epl_1617.mean()[1]) for i in range(-6,8)]
+
+plt.hist(epl_1617[['HomeGoals']].values - epl_1617[['AwayGoals']].values, range(-6,8), 
+         alpha=0.7, label='Actual',normed=True)
+plt.plot([i+0.5 for i in range(-6,8)], skellam_pred,
+                  linestyle='-', marker='o',label="Skellam", color = '#CD5C5C')
+plt.legend(loc='upper right', fontsize=13)
+plt.xticks([i+0.5 for i in range(-6,8)],[i for i in range(-6,8)])
+plt.xlabel("Home Goals - Away Goals",size=13)
+plt.ylabel("Proportion of Matches",size=13)
+plt.title("Difference in Goals Scored (Home Team vs Away Team)",size=14,fontweight='bold')
+plt.ylim([-0.004, 0.26])
+plt.tight_layout()
+plt.show()
+
+
+
+
+""" ######################## """
+""" Poisson for goals / half """
+""" ######################## """
+
+epl_1617_halves = pd.read_csv("http://www.football-data.co.uk/mmz4281/1617/E0.csv")
+epl_1617_halves = epl_1617_halves[['FTHG', 'FTAG', 'HTHG', 'HTAG']]
+epl_1617_halves['FHgoals'] = epl_1617_halves['HTHG'] + epl_1617_halves['HTAG']
+epl_1617_halves['SHgoals'] = epl_1617_halves['FTHG'] + epl_1617_halves['FTAG'] - epl_1617_halves['FHgoals']
+epl_1617_halves = epl_1617_halves[['FHgoals', 'SHgoals']]
+epl_1617_halves.mean()
+
+poisson_halves_pred = np.column_stack([[poisson.pmf(i, epl_1617_halves.mean()[j]) for i in range(8)] for j in range(2)])
+
+plt.hist(epl_1617_halves.values, range(9), 
+         alpha=0.7, label=['1st Half', '2nd Half'],normed=True, color=["#FFA07A", "#20B2AA"])
+
+pois1, = plt.plot([i-0.5 for i in range(1,9)], poisson_halves_pred[:,0],
+                  linestyle='-', marker='o',label="1st Half", color = '#CD5C5C')
+pois2, = plt.plot([i-0.5 for i in range(1,9)], poisson_halves_pred[:,1],
+                  linestyle='-', marker='o',label="2nd Half", color = '#006400')
+
+leg=plt.legend(loc='upper right', fontsize=13, ncol=2)
+leg.set_title("Poisson              Actual        ", prop = {'size':'14', 'weight':'bold'})
+
+plt.xticks([i-0.5 for i in range(1,9)],[i for i in range(9)])
+plt.xlabel("Goals per Half",size=13)
+plt.ylabel("Proportion of Matches",size=13)
+plt.title("Number of Goals per Half (EPL 2016/17 Season)",size=14,fontweight='bold')
+plt.ylim([-0.004, 0.4])
+plt.tight_layout()
+plt.show()
+
+
+""" ################# """
+""" calcutate results """
+""" ################# """
+
+# importing the tools required for the Poisson regression model
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+
+goal_model_data = pd.concat([epl_1617[['HomeTeam','AwayTeam','HomeGoals']].assign(home=1).rename(
+            columns={'HomeTeam':'team', 'AwayTeam':'opponent','HomeGoals':'goals'}),
+           epl_1617[['AwayTeam','HomeTeam','AwayGoals']].assign(home=0).rename(
+            columns={'AwayTeam':'team', 'HomeTeam':'opponent','AwayGoals':'goals'})])
+
+poisson_model = smf.glm(formula="goals ~ home + team + opponent", data=goal_model_data, 
+                        family=sm.families.Poisson()).fit()
+
+print(poisson_model.summary())
+
+print(poisson_model.predict(pd.DataFrame(data={'team': 'Chelsea', 'opponent': 'Sunderland',
+                                       'home':1},index=[1])))
+
+print(poisson_model.predict(pd.DataFrame(data={'team': 'Sunderland', 'opponent': 'Chelsea',
+                                       'home':0},index=[1])))
+
+def simulate_match(foot_model, homeTeam, awayTeam, max_goals=10):
+    home_goals_avg = foot_model.predict(pd.DataFrame(data={'team': homeTeam, 
+                                                            'opponent': awayTeam,'home':1},
+                                                      index=[1])).values[0]
+    away_goals_avg = foot_model.predict(pd.DataFrame(data={'team': awayTeam, 
+                                                            'opponent': homeTeam,'home':0},
+                                                      index=[1])).values[0]
+    team_pred = [[poisson.pmf(i, team_avg) for i in range(0, max_goals+1)] for team_avg in [home_goals_avg, away_goals_avg]]
+    return(np.outer(np.array(team_pred[0]), np.array(team_pred[1])))
+
+
+""" This matrix simply shows the probability of Chelsea (rows of the matrix) and Sunderland 
+    (matrix columns) scoring a specific number of goals. For example, along the diagonal, 
+    both teams score the same the number of goals (e.g. P(0-0)=0.031). So, you can calculate 
+    the odds of draw by summing all the diagonal entries. Everything below the diagonal 
+    represents a Chelsea victory (e.g P(3-0)=0.149), And you can estimate P(Over 2.5 goals) 
+    by summing all entries except the four values in the upper left corner. """
+
+print(simulate_match(poisson_model, 'Chelsea', 'Sunderland', max_goals=3))
+
+
+""" only result """
+
+chel_sun = simulate_match(poisson_model, "Chelsea", "Sunderland", max_goals=10)
+# chelsea win
+print(np.sum(np.tril(chel_sun, -1)))
+# draw
+print(np.sum(np.diag(chel_sun)))
+# sunderland win
+print(np.sum(np.triu(chel_sun, 1)))
+
+
+
