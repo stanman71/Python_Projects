c6b3aac30752bfa2acf888b4f103cad3d376363b
stanman71 <stanlay@gmx.net>
2019-02-09 18:06:26 +0100

    add light sleep

--- RasPi/SmartHome/ardiuno/ESP8266/ESP8266.ino
+++ RasPi/SmartHome/ardiuno/ESP8266/ESP8266.ino
@@ -6,51 +6,62 @@
 ESP8266WiFiMulti WiFiMulti;
 
 void setup() 
-{
-  Serial.begin(115200);
 
-  // boot break (4s)
-  for(uint8_t t = 4; t > 0; t--) 
-  {
-    Serial.printf("SETUP WAIT %d...\n", t);
-    Serial.flush();
-    delay(1000);
-  }
+    {
+      Serial.begin(115200);
 
-  // SSID, Password
-  WiFiMulti.addAP("Stanlay", "");
-}
+      // boot break (4s)
+      for(uint8_t t = 4; t > 0; t--) 
+
+          {
+            Serial.printf("SETUP WAIT %d...\n", t);
+            Serial.flush();
+            delay(1000);
+          }
+
+      // SSID, Password
+      WiFiMulti.addAP("Stanlay", ""); 
+      
+      //  Force the ESP into client-only mode
+      WiFi.mode(WIFI_STA); 
+
+      //  Enable light sleep
+      wifi_set_sleep_type(LIGHT_SLEEP_T);
+    }
 
 
 void loop() 
-{
 
-  if((WiFiMulti.run() == WL_CONNECTED)) 
+    {
 
-      {
-        Serial.println("WiFi connected");
+      if((WiFiMulti.run() == WL_CONNECTED)) 
 
-        // read sensor
-        int sensorValue = analogRead(A0);
-        float voltage = sensorValue * (3.2 / 1023.0);
-        
-        Serial.println(voltage);
+          {
+            Serial.println("WiFi connected");
 
-        String voltage_result;
-        voltage_result = String(voltage);
+            // read sensor
+            int sensorValue = analogRead(A0);
+            float voltage = sensorValue * (3.2 / 1023.0);
+            
+            Serial.println(voltage);
 
-        // transfer values to the raspberry
-        HTTPClient http;
-        http.begin("http://192.168.1.40:5000/mqtt/1/sensor/" + voltage_result);
-        int httpCode = http.GET();
-        http.end();
+            String voltage_result;
+            voltage_result = String(voltage);
 
-        delay(10000);
-      }
+            // transfer values to the raspberry
+            HTTPClient http;
+            http.begin("http://192.168.1.20:5000/mqtt/1/sensor/" + voltage_result);
+            int httpCode = http.GET();
+            http.end();
 
-  else
+            // sleep 1 hour
+            delay(3600000);
+          }
 
-      {
-        Serial.println("No WiFi");
-      }
-}
+      else
+
+          {
+            Serial.println("No WiFi");
+          }
+
+    }
